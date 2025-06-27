094c305bf72327430fc69930b0dd7b752dcf3a2b
Stevetatu <stevetatu3@gmail.com>
2025-06-27 22:33:18 +0300

    chapter one of rust lang book

---
+++ first_project/target/debug/deps/first_project-6275712befa75f9a.d
@@ -0,0 +1,5 @@
+/home/offensive/ice9/first_project/target/debug/deps/first_project-6275712befa75f9a.d: src/main.rs
+
+/home/offensive/ice9/first_project/target/debug/deps/first_project-6275712befa75f9a: src/main.rs
+
+src/main.rs:
---
+++ first_project/target/debug/first_project.d
@@ -0,0 +1,1 @@
+/home/offensive/ice9/first_project/target/debug/first_project: /home/offensive/ice9/first_project/src/main.rs
